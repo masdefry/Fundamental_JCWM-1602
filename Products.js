473f231952968dceb615eb12f6a0953d959fbfbd
masdefry <muhammaddefryan@gmail.com>
2021-02-08 11:50:28 +0700

    Add Data Product & Delete Data Product

--- Products.js
+++ Products.js
@@ -27,7 +27,7 @@ function showDataProducts(){
                 <td>
                     <center>
                         <input type="button" name="button-edit" value="Edit">
-                        <input type="button" name="button-delete" value="Delete">
+                        <input type="button" name="button-delete" value="Delete" onClick="deleteProduct(${i})">
                     </center>
                 </td>
             </tr> 
@@ -38,3 +38,45 @@ function showDataProducts(){
 }
 
 showDataProducts()
+
+function addDataProduct(){
+    let inputs = document.getElementsByClassName('input-product')
+    console.log(inputs)
+
+    let productName = inputs[0].value
+    let price = inputs[1].value
+    let stock = inputs[2].value
+    let imageURL = inputs[3].value
+    
+    if(productName && price && stock && imageURL){
+        dataProducts.push({
+            name: productName,
+            price: price,
+            stock: stock,
+            imageURL: imageURL
+        })
+
+        showDataProducts()
+
+        inputs[0].value = ''
+        inputs[1].value = ''
+        inputs[2].value = ''
+        inputs[3].value = ''
+    }else{
+        let errorMessage = document.getElementById('error-message')
+        errorMessage.innerHTML = 'Data Must Be Filled!'
+    }
+}
+
+document.getElementById('button-submit').addEventListener('click', addDataProduct)
+
+function deleteProduct(i){
+    let confirmBox = confirm(`Are you sure want to delete product ${dataProducts[i].name}?`)
+
+    if(confirmBox){
+        dataProducts.splice(i, 1)
+        alert('Delete Product Success!')
+
+        showDataProducts()
+    }
+}
